408b69a548eeedec51a71f0ea0ed1b7203d2c2db
Takeru Kobayashi <31469598+takeru1990@users.noreply.github.com>
2022-09-21 21:45:36 +0900

    Ubuntu setting up script

---
+++ ini-ubuntu.sh
@@ -0,0 +1,32 @@
+# Ubuntu initial setting 
+
+# Connect to Ubuntu LivePatch 
+sudo ua attach [YOUR TOKEN]
+
+# Change directory name
+LANG=C xdg-user-dirs-gtk-update
+
+# snap app install
+sudo snap install gnome-text-editor
+sudo snap install gimp
+sudo snap install inkscape
+sudo snap install kdenlive
+sudo snap install obs-studio
+sudo snap install slack
+sudo snap install blender --classic
+sudo snap install audacity
+sudo snap install gnome-boxes
+
+
+# APT app install
+sudo apt install ffmpeg
+sudo apt install fontforge
+sudo apt install git
+sudo apt install gnome-tweaks
+sudo apt install kazam
+sudo apt install pavucontrol
+
+
+# Install manually
+# Google Chrome
+
