db791bedb691568c936d1db2617e1262b7d3449d
Takeru Kobayashi <31469598+takeru1990@users.noreply.github.com>
2022-09-26 00:22:09 +0900

    use apt instead snap for Inkscape install
    detail explained on https://lookbackmargin.blog/2022/09/25/snap-obs-saving-directory/

--- ini-ubuntu.sh
+++ ini-ubuntu.sh
@@ -10,7 +10,6 @@ LANG=C xdg-user-dirs-gtk-update
 # snap app install
 sudo snap install gnome-text-editor
 sudo snap install gimp
-sudo snap install inkscape
 sudo snap install kdenlive
 sudo snap install obs-studio
 sudo snap install slack
@@ -24,6 +23,7 @@ sudo apt install ffmpeg
 sudo apt install fontforge
 sudo apt install git
 sudo apt install gnome-tweaks
+sudo apt install inkscape
 sudo apt install kazam
 sudo apt install pavucontrol
 
