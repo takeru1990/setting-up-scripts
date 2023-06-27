983847e7984875be0f3faf3441c7a4dd66366a7b
Takeru Kobayashi <31469598+takeru1990@users.noreply.github.com>
2023-06-28 00:16:32 +0900

    Create setup-fedora.sh
    First draft

---
+++ setup-fedora.sh
@@ -0,0 +1,28 @@
+# Enable RPMFusion repositories
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Install plugins for playing movies and music
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional Multimedia
+
+# Install librosa for Pitivi
+pip install librosa
+
+# Install DNF Apps
+sudo dnf install gnome-tweaks
+sudo dnf install inkscape
+sudo dnf install gimp
+sudo dnf install obs-studio
+sudo dnf install pitivi
+sudo dnf install gnome-extensions-app
+sudo dnf install ibus-mozc langpacks-ja
+
+# Install Ubuntu fonts
+wget https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip
+unzip 0cef8205-ubuntu-font-family-0.83.zip
+mkdir ~/.fonts
+mv ~/ubuntu-font-family-0.83/*.ttf ~/.fonts/
